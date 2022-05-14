748bb2577474ff9854bbde59717bdfa9b8089c20
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2022-05-14 10:55:38 -0400

    cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 import { routes } from './routes';
 
 const app = express();
 
+var corsOptions = {
+  origin: [
+    'http://localhost:3333',
+    'https://feedbackwidget-production-5b18.up.railway.app',
+  ],
+  optionsSuccessStatus: 200, // For legacy browser support
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(routes);
 
