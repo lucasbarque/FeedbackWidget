d5b20d5ba0c19736addcf0645b5417c5276c54e5
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2022-05-14 11:25:56 -0400

    body-parser

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import { routes } from './routes';
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
 
 app.use(cors());
 app.use(express.json());
